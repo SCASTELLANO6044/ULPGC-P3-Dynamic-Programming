c8574d6eb8d4499fe6661a0f480a8a262acf743e
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-27 23:37:38 +0000

    .

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello world')
+# Dictionary with Weight in first column and Benefit in second column
+data = {
+    2: 3,
+    3: 4,
+    4: 5,
+    5: 6
+}
+# Maximum size availabe in the backpack
+max_size = 5
+
