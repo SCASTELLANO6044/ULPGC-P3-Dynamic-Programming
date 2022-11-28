0108919592be441a248ed591ac1dfd92a5b85c39
casteser <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-28 11:12:59 +0000

    Deleted comented code

--- not consecutive same value backtracking/main.py
+++ not consecutive same value backtracking/main.py
@@ -1,28 +1,3 @@
-"""
-result = []
-max_size = 3
-
-arrayL = []
-arrayR = []
-
-def treeSearch():
-    comb = []
-    for i in range(2):
-        if isValid(comb, i) == True:
-            comb.append(i)
-            treeSearch()
-        
-
-def isValid(array, val):
-    if (len(array) == 0 or array[len(array)-1] == val):
-        return False
-    else:
-        return True
-
-treeSearch()
-print(result)
-"""
-
 result = []
 depth = 0
 def searchTree(result):
