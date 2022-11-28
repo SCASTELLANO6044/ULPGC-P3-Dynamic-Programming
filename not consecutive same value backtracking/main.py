280a98fba347b0a802475045bbcc65701ce735fa
casteser <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-11-28 11:24:42 +0000

    Format

--- not consecutive same value backtracking/main.py
+++ not consecutive same value backtracking/main.py
@@ -1,28 +1,30 @@
 result = []
 depth = 0
-def searchTree(result):
+
+
+def searchtree(solution):
     res = []
     global depth
-    for i in range (2):
-        if ((isValid(result, i) == True) and (depth < 3)):
+    for i in range(2):
+        if (isvalid(solution, i)) and (depth < 3):
             depth = depth + 1
             res.append(i)
-            aux = searchTree(res)
-            if aux != None:
+            aux = searchtree(res)
+            if aux is not None:
                 res.append(aux)
-    depth = depth -1
-    if (len(res) > 0):
+    depth = depth - 1
+    if len(res) > 0:
         return res
     else:
         return
 
-def isValid(result, val):
-    if (len(result)==0 or result[len(result)-1] != val):
+
+def isvalid(solution, val):
+    if len(solution) == 0 or solution[len(solution) - 1] != val:
         return True
     else:
         return False
 
-result.append(searchTree(result))
 
-
+result.append(searchtree(result))
 print(result)
