54438f23a3c60ba4aa3a83f8bea41785afd4d0c8
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-12-04 18:38:47 +0000

    Update fibo-tabulation.py

--- Fibonacci tabulation/fibo-tabulation.py
+++ Fibonacci tabulation/fibo-tabulation.py
@@ -1,14 +1,13 @@
 
-def Fib(n):
+def fib(n):
     if n < 2:
         return n
     else:
-        table = []
-        table.append(0)
-        table.append(1)
+        table = [0, 1]
 
-        for j in range(2,n+1):
+        for j in range(2, n+1):
             table.append(table[j-2]+table[j-1])
         return table[len(table)-1]
 
-print(Fib(8))
+
+print(fib(8))
