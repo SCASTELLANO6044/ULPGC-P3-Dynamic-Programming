d9de0359dafdbc87b4dfc45d94eb3710f05d02ec
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-12-05 20:53:53 +0000

    Setting up project in java

---
+++ knaspsack tabulation/java-project/src/main/java/package1/JavaProject.java
@@ -0,0 +1,12 @@
+package package1;
+
+/**
+ *
+ * @author scast
+ */
+public class JavaProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
