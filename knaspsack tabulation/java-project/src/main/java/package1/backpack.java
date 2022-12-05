d927e0aaffe6cdeda7bf6e9e67f5b68beefbc234
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-12-05 21:11:55 +0000

    Update backpack.java

--- knaspsack tabulation/java-project/src/main/java/package1/backpack.java
+++ knaspsack tabulation/java-project/src/main/java/package1/backpack.java
@@ -1,7 +1,30 @@
 package package1;
 
 public class backpack {
-    public backpack(){
+    
+    private int [][]items;
+    private int maxWreight;
+    private int numItems;
+    
+    public backpack(int[][] items, int maxWeight){
+        this.items=items;
+        this.maxWreight=maxWeight;
+        this.numItems = items.length;
+    }
+
+    public int[][] getItems() {
+        return items;
+    }
+
+    public int getMaxWreight() {
+        return maxWreight;
+    }
+
+    public int getNumItems() {
+        return numItems;
+    }
+    
+    public void getBestCombination(){
         
     }
 }
