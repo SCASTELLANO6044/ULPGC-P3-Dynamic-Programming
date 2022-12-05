ca9bb0eca7a6a87d5fb36114f51ee5f21c88df46
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-12-05 21:59:33 +0000

    Update backpack.java

--- knaspsack tabulation/java-project/src/main/java/package1/backpack.java
+++ knaspsack tabulation/java-project/src/main/java/package1/backpack.java
@@ -4,6 +4,7 @@ public class backpack {
     
     private int [][] items;
     private final int maxWreight;
+    private int actualWeight;
     private final int numItems;
     private int [][] table;
     
@@ -11,7 +12,8 @@ public class backpack {
         this.items=items;
         this.maxWreight=maxWeight;
         this.numItems = items.length;
-        this.table = new int [6][5];
+        this.table = new int [5][6];
+        this.actualWeight = 0;
     }
 
     public int[][] getItems() {
@@ -29,13 +31,27 @@ public class backpack {
     public void getBestCombination(){
         // Caso base 1 si numItems <=0 funcion = 0
         for (int i = 0; i < this.table[0].length; i++) {
-            this.table[0][i] = 1;
+            this.table[0][i] = 0;
         }
         //Caso base la mochila no tiene capacidad.
         for (int i = 0; i < this.table.length; i++) {
-            this.table[i][0] = 1;
+            this.table[i][0] = 0;
         }
-        debugArray(table);
+        
+        //Caso general escogemos opción con mayor beneficio.
+        for (int i = 1; i < table.length; i++) {
+            for (int j = 0; j < table[0].length; j++) {
+                if (j <= table[0].length){
+                    if ((items[i-1][1] + table[i-1][maxWreight-j]) > table[i-1][maxWreight]){
+                        table[i][j] = items[i-1][1] + table[i-1][maxWreight-j];
+                    }else{
+                        table[i][j] = table[i-1][maxWreight];
+                    }
+                }else{
+                    table[i][j] = table[i-1][j];
+                }
+            }
+        }
         
     }
     
@@ -44,6 +60,7 @@ public class backpack {
             for (int j = 0; j < array[0].length; j++) {
                 System.out.println(array[i][j]);
             }
+            System.out.println("-------");
         }
     }
 }
