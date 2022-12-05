a9c45c33d7300afd7997dfefc8ae239cf32ed00b
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-12-05 21:06:03 +0000

    .

--- knaspsack tabulation/java-project/src/main/java/package1/JavaProject.java
+++ knaspsack tabulation/java-project/src/main/java/package1/JavaProject.java
@@ -1,12 +1,26 @@
 package package1;
 
-/**
- *
- * @author scast
- */
 public class JavaProject {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int [][] items = new int [4][2];
+        
+        items [0][0] = 2;
+        items [0][1] = 3;
+        
+        items [1][0] = 3;
+        items [1][1] = 4;
+        
+        items [2][0] = 4;
+        items [2][1] = 5;
+        
+        items [3][0] = 5;
+        items [3][1] = 6;
+        
+        for (int i = 0; i < items.length; i++) {
+            for (int j = 0; j < items[0].length; j++) {
+                System.out.println(items[i][j]);
+            }
+        }
     }
 }
