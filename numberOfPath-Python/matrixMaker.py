f82ce615533a29fa20824d369b5085c7b60e26b6
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2022-12-12 22:26:52 +0000

    Update matrixMaker.py

--- numberOfPath-Python/matrixMaker.py
+++ numberOfPath-Python/matrixMaker.py
@@ -2,6 +2,10 @@ class MatrixMaker(object):
 
     @staticmethod
     def createMatrix(fileName):
+        file = open(fileName, 'r')
+        Lines = file.readlines()
+        # for line in Lines:
+            
         print('hello')
 
     @staticmethod
