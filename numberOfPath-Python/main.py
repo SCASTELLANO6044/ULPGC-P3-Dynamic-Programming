40132a6ab221721f79e1e040860f6503229f4ef3
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-04 18:28:38 +0000

    Añadiendo switches

--- numberOfPath-Python/main.py
+++ numberOfPath-Python/main.py
@@ -1,3 +1,8 @@
+tab = False
+mem = False
+both = False
+isDirectory = False
+
 print("Uso del programa: NumberOfPaths.py [-d [DIRECTORY] | -f [FILE]] [-sm | -st | -check] [-t]\n\n" +
         "optional arguments:\n" +
         "    -d [DIRECTORY], --directory [DIRECTORY]     process many files in a directory\n" +
@@ -12,7 +17,18 @@ print("Uso del programa: NumberOfPaths.py [-d [DIRECTORY] | -f [FILE]] [-sm | -s
 
 First_param = input("If you are going to use a file please enter F\n"+
                      "If you are going to use a directory please enter D\n"+
-                     "Option: ")
-Second_param = input("Please enter the route to the file or direcotry we are going to use:\n"+
-                     "Route: ")
+                     "Opción: ")
+if First_param == 'd' or 'D':
+    isDirectory = True
+    second_param = input("Por favor introduce la ruta al directorio:\n"+
+                         "Ruta: ")
+elif First_param == 'f' or 'F':
+    second_param = input("Por favor introduce la ruta al archivo:\n" +
+                         "Ruta: ")
+Third_param = input("Por favor, introduzca alguna de las siguientes opciones:\n"+
+                    "sm -> para contar el número de caminos mediante memoization,\n"+
+                    "st -> para contar el número de caminos mediante tabulation, o,\n"+
+                    "check -> para contar el número de caminos por ambos métodos.\n"+
+                    "Opción: ")
+
 # Keyboard inputs
