0b1fe5494628aa38428e95f1ba13b27a48cc36a0
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-14 22:30:51 +0100

    better output

--- Fibonacci-memoization/main.py
+++ Fibonacci-memoization/main.py
@@ -4,14 +4,16 @@ def fib(n):
 
     def mem_fib(key):
         if key not in mem:
+            print("key NOT found in mem")
             if key < 2:
                 res = key
             else:
                 res = fib(key - 1) + fib(key - 2)
             mem[key] = res
+        print("key found in mem")
         return mem[key]
 
     return mem_fib(n)
 
 
-print(fib(6))
+print(fib(50))
