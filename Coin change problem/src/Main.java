5bc329a8e295afbc72d22d7ffa9ff3c1562ab21f
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-15 19:50:13 +0100

    First approach to coin change problem

--- Coin change problem/src/Main.java
+++ Coin change problem/src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer [] coins = {1, 2, 3};
+        System.out.println(Tabulation.calculate(coins, 4));
+
+        int coinTypeAmount = 3;
+        int sum = 9;
+        int[][] dp = new int[coinTypeAmount + 1][sum + 1];
+        for (int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        int res = Memoization.calculate(coins, sum, coinTypeAmount, dp);
+        System.out.println(res);
+        System.out.println(Arrays.deepToString(dp));
     }
 }
