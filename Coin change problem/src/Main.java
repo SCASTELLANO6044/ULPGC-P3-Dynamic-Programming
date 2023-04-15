f9786a474dcd8ebd18c44044703d5c86482be028
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-15 21:55:52 +0100

    now show combinations on memoization

--- Coin change problem/src/Main.java
+++ Coin change problem/src/Main.java
@@ -1,26 +1,42 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Integer [] coins = {1, 2, 3};
-        Map<String, Integer> memory = new HashMap<>();
-        System.out.println(memoization(coins, coins.length-1, 4, memory));
-        System.out.println(tabulation(coins, 9));
+        Map<String, List<List<Integer>>> memory = new HashMap<>(); // Cambio de valor de retorno a lista de listas
+        List<List<Integer>> combinations = memoization(coins, coins.length-1, 4, memory);
+        Integer numOfCombinations = combinations.size();
+        System.out.println(numOfCombinations);
+        for (List<Integer> combination : combinations) {
+            System.out.println(combination);
+        }
     }
 
-    private static Integer memoization (Integer[] coins, Integer coinTypeNum, Integer sum, Map<String, Integer> memory){
+    private static List<List<Integer>> memoization (Integer[] coins, Integer coinTypeNum, Integer sum, Map<String, List<List<Integer>>> memory){
         if (sum == 0){
-            return 1;
+            List<List<Integer>> combinations = new ArrayList<>();
+            combinations.add(new ArrayList<>());
+            return combinations;
         }
         if (sum < 0 || coinTypeNum < 0){
-            return 0;
+            List<List<Integer>> combinations = new ArrayList<>();
+            return combinations;
         }
         String key = coinTypeNum + "->"+sum;
         if (!memory.containsKey(key)){
-            Integer taken = memoization(coins, coinTypeNum, sum - coins[coinTypeNum], memory);
-            Integer notTaken = memoization(coins, coinTypeNum-1, sum, memory);
-            memory.put(key, taken+notTaken);
+            List<List<Integer>> takenCombinations = memoization(coins, coinTypeNum, sum - coins[coinTypeNum], memory);
+            List<List<Integer>> notTakenCombinations = memoization(coins, coinTypeNum-1, sum, memory);
+            List<List<Integer>> combinations = new ArrayList<>();
+            for (List<Integer> combination : takenCombinations) {
+                List<Integer> takenCombination = new ArrayList<>(combination);
+                takenCombination.add(coins[coinTypeNum]);
+                combinations.add(takenCombination);
+            }
+            combinations.addAll(notTakenCombinations);
+            memory.put(key, combinations);
         }
         return memory.get(key);
     }
