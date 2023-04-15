2bb96a076a667cbf72b2f76a6419212a8536eb55
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-15 20:53:39 +0100

    memoization and tabulation

--- Coin change problem/src/Main.java
+++ Coin change problem/src/Main.java
@@ -1,18 +1,50 @@
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         Integer [] coins = {1, 2, 3};
-        System.out.println(Tabulation.calculate(coins, 4));
+        Map<String, Integer> memory = new HashMap<>();
+        System.out.println(memoization(coins, coins.length-1, 4, memory));
+        System.out.println(tabulation(coins, 9));
+    }
 
-        int coinTypeAmount = 3;
-        int sum = 9;
-        int[][] dp = new int[coinTypeAmount + 1][sum + 1];
-        for (int[] row : dp){
-            Arrays.fill(row, -1);
+    private static Integer memoization (Integer[] coins, Integer coinTypeNum, Integer sum, Map<String, Integer> memory){
+        if (sum == 0){
+            return 1;
         }
-        int res = Memoization.calculate(coins, sum, coinTypeAmount, dp);
-        System.out.println(res);
-        System.out.println(Arrays.deepToString(dp));
+        if (sum < 0 || coinTypeNum < 0){
+            return 0;
+        }
+        String key = coinTypeNum + "->"+sum;
+        if (!memory.containsKey(key)){
+            Integer taken = memoization(coins, coinTypeNum, sum - coins[coinTypeNum], memory);
+            Integer notTaken = memoization(coins, coinTypeNum-1, sum, memory);
+            memory.put(key, taken+notTaken);
+        }
+        return memory.get(key);
+    }
+
+    private static Integer tabulation (Integer[] coins, Integer sum){
+        int n = coins.length;
+
+        int[][] table = new int[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++) {
+            table[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (coins[i - 1] > j) {
+                    table[i][j] = table[i - 1][j];
+                }
+                else {
+                    table[i][j] = table[i - 1][j] + table[i][j - coins[i - 1]];
+                }
+            }
+        }
+
+        return table[n][sum];
     }
 }
