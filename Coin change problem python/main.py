08e7fa18dc0277e6108bb462ae8a997799acc60c
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-04-16 15:01:10 +0100

    adding flags control

--- Coin change problem python/main.py
+++ Coin change problem python/main.py
@@ -45,6 +45,16 @@ def getCombinations(table, coins, i, j):
             j -= coins[i-1]
     return combinations
 
+# Inicio del programa.
+
+tab = False
+mem = False
+both = False
+isDirectory = False
+
+valid_first = ["-d", "--directory", "-f", "--file", "-h", "--help"]
+valid_second = ["-sm", "--memoization", "-st", "--tabulation", "--check"]
+
 if sys.argv[1] == "-h" or sys.argv[1] == "--help":
     print("Uso del programa: CoinChangeProblem.py [-d [DIRECTORY] | -f [FILE]] [-sm | -st | -check]\n\n" +
           "optional arguments:\n" +
@@ -54,6 +64,38 @@ if sys.argv[1] == "-h" or sys.argv[1] == "--help":
           "    -st, --tabulation                           count number of combinations to reach the change with the available coins through Tabulation\n" +
           "    --check                                     check that the count number of combinations to reach the change with the available coins\n" +
           "                                                    is the same through Tabulation and Memoization\n")
+elif sys.argv[1] not in valid_first or sys.argv[3] not in valid_second:
+    print("No ha escrito los parámetros de entrada del programa correctamente:\n"
+          "Uso del programa: CoinChangeProblem.py [-d [DIRECTORY] | -f [FILE]] [-sm | -st | -check]\n\n" +
+          "optional arguments:\n" +
+          "    -d [DIRECTORY], --directory [DIRECTORY]     process many files in a directory\n" +
+          "    -f [FILE], --file [FILE]                    process a single file\n" +
+          "    -sm, --memoization                          count number of combinations to reach the change with the available coins through Memoization\n" +
+          "    -st, --tabulation                           count number of combinations to reach the change with the available coins through Tabulation\n" +
+          "    --check                                     check that the count number of combinations to reach the change with the available coins\n" +
+          "                                                    is the same through Tabulation and Memoization\n")
+else:
+    second_param = ""
+
+    if sys.argv[1] == valid_first[0] or sys.argv[1] == valid_first[1]:
+        isDirectory = True
+        second_param = sys.argv[2]
+        print(second_param)
+    elif sys.argv[1] == valid_first[2] or sys.argv[1] == valid_first[3]:
+        second_param = sys.argv[2]
+
+    if sys.argv[3] == valid_second[4]:
+        tab = False
+        mem = False
+        both = True
+    elif sys.argv[3] == valid_second[0] or sys.argv[3] == valid_second[1]:
+        tab = False
+        mem = True
+        both = False
+    elif sys.argv[3] == valid_second[2] or sys.argv[3] == valid_second[3]:
+        tab = True
+        mem = False
+        both = False
 
 
 coins = [1, 2, 3]
