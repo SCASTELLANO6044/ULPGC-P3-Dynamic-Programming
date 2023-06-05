9334a55ea5b886548577ae3fd9615d5fe1bef804
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2023-06-05 20:33:50 +0100

    :sparkles: Reading input from files

--- Coin change problem/src/main/Main.java
+++ Coin change problem/src/main/Main.java
@@ -2,19 +2,22 @@ package main;
 
 import dynamicprogramming.Memoization;
 import dynamicprogramming.Tabulation;
+import utils.Description;
+import utils.FileInputReader;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        int [] coins = {1, 2, 3};
-        Map<String, Integer> memory = new HashMap<>();
+        Description description = new FileInputReader().read("C:\\GitHubProjects\\ULPGC-P3-Dynamic-Programming\\Coin change problem\\src\\input\\1.txt");
 
-        int numOfCombinations = Memoization.execute(coins, coins.length-1, 4, memory);
+        Map<String, Integer> memory = new HashMap<>();
+        int numOfCombinations = Memoization.execute(description.getCoinTypeArray(),
+                description.getCoinTypeArray().length - 1, description.getSum(), memory);
         System.out.println(numOfCombinations);
 
-        int tabulation = Tabulation.execute(coins, 4);
+        int tabulation = Tabulation.execute(description.getCoinTypeArray(), description.getSum());
         System.out.println(tabulation);
     }
 }
---
+++ Coin change problem/src/utils/Description.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class Description {
+    private int sum;
+    private int[] coinTypeArray;
+
+    public Description(int sum, int[] coinTypeArray) {
+        this.sum = sum;
+        this.coinTypeArray = coinTypeArray;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public int[] getCoinTypeArray() {
+        return coinTypeArray;
+    }
+}
