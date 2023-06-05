a2bba10cdf94730ece410438bb0e5bf3b808017e
SCASTELLANO6044 <sergiocastellanotrabajo@gmail.com>
2023-06-05 20:41:25 +0100

    :tada:

--- Coin change problem/src/utils/FileInputReader.java
+++ Coin change problem/src/utils/FileInputReader.java
@@ -5,6 +5,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileInputReader {
+
+    public FileInputReader(){}
     public static Description read(String file){
 
         Description descripcion = null;
